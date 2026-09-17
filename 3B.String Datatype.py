b52541c6f6e9d2cfb2c8c93d1e8ae92840df1551
Pragathi.L <lpragathi486@gmail.com>
2026-09-17 12:24:08 +0530

    Add files via upload

---
+++ 3B.String Datatype.py
@@ -0,0 +1,29 @@
+first_name="Pragathi"
+last_name="Pai"
+full_name=first_name + " "+ last_name
+print(full_name)
+
+
+message =  "warning! "
+print(message*3)
+print(message.upper())
+print(message.lower())
+print(message.strip()*3)
+print(message.replace("warning","error"))
+
+name="""pragathi said 'hello'
+        prajwal said 'hii'"""
+print(name)
+
+print(len(message))
+
+name="pragathi"
+print(name[3])
+print(name[2:6])
+print(name[:6])
+print(name[2:])
+print(name[-3])
+print(name[::2])
+
+s="Pragathi \\is a good girl"
+print(s)
