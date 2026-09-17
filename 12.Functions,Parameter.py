cbc0cdf49b7ce56d7dfc1a1b9bcba28dc55c7237
Pragathi.L <lpragathi486@gmail.com>
2026-09-17 12:27:21 +0530

    Add files via upload

---
+++ 12.Functions,Parameter.py
@@ -0,0 +1,50 @@
+#functions
+
+# def greet():  #defining fnction
+#     print("Hello, Good morning!")
+# greet()    #calling function
+
+
+# def marriage(boy,girl):  #parameter
+#     print(f"boy is {boy}")
+#     print(f"girl id {girl}")
+#     print(f"{boy} married {girl}")
+# marriage("vikas","sneha")   #positional arguments
+# marriage(boy="chnadan",girl="megha")   #keyword arguments
+
+
+# for i in range(1,11):
+#     print(f"2X{i}={2*i}")
+
+
+# def tables(num):
+#     for i in range(1,11):
+#         print(f"{num}X{i}={num*i}")
+# tables(44)
+
+
+# def marriage(boy,girl="girl"):  #default paramter
+#     print(f"boy is {boy}")
+#     print(f"girl id {girl}")
+#     print(f"{boy} married {girl}")
+# marriage("vikas")  
+
+
+def func(num):
+    return int(str(num)*5)  #returning values
+a=100
+b=func(2)
+c=a+b
+print(c)
+
+
+# def func():
+#     x="pyhton"    #local variable
+#     print("hello python")
+#     print(y)
+# y="html"     #global variable
+# print(x)
+
+
+
+
